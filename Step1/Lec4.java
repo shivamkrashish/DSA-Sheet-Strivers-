715c3a400a652d04147625ae3d853746525df4b2
shivamkrashish <shivamkumar120904@gmail.com>
2025-05-26 15:58:56 +0530

    DSA-Sheet-Strivers

--- Step1/Lec4.java
+++ Step1/Lec4.java
@@ -92,24 +92,78 @@
 //You are given an integer n. You need to check whether it is an armstrong number or not.
 //Return true if it is an armstrong number, otherwise return false.
 //An armstrong number is a number which is equal to the sum of the digits of the number, raised to the power of the number of digits.
-public class Lec4 {
-    public static boolean isArmstrong(int n) {
-        int original = n;
-        int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit * digit;
-            n /= 10;
-        }
-        return sum == original;
-    }
+// public class Lec4 {
+//     public static boolean isArmstrong(int n) {
+//         int original = n;
+//         int sum = 0;
+//         while (n > 0) {
+//             int digit = n % 10;
+//             sum += digit * digit * digit;
+//             n /= 10;
+//         }
+//         return sum == original;
+//     }
 
-    public static void main(String[] args) {
-        int n = 103;
-        if (isArmstrong(n)) {
-            System.out.println(n + " is an Armstrong number.");
-        } else {
-            System.out.println(n + " is NOT an Armstrong number.");
+//     public static void main(String[] args) {
+//         int n = 103;
+//         if (isArmstrong(n)) {
+//             System.out.println(n + " is an Armstrong number.");
+//         } else {
+//             System.out.println(n + " is NOT an Armstrong number.");
+//         }
+//     }
+// }
+
+
+//Q6.Divisors of a Number
+// You are given an integer n. You need to find all the divisors of n. 
+//Return all the divisors of n as an array or list in a sorted order.
+// A number which completely divides another number is called it's divisor.
+// import java.util.ArrayList;
+// import java.util.Collections;
+// public class Lec4 {
+//     public static ArrayList<Integer> getDivisors(int n) {
+//         ArrayList<Integer> divisors = new ArrayList<>();
+//         for (int i = 1; i <= Math.sqrt(n); i++) {
+//             if (n % i == 0) {
+//                 divisors.add(i);
+//                 if (i != n / i) {
+//                     divisors.add(n / i);
+//                 }
+//             }
+//         }
+//         // Sorting the divisors in ascending order
+//         Collections.sort(divisors);
+//         return divisors;
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 36;
+//         ArrayList<Integer> result = getDivisors(n);
+//         System.out.println("Divisors of " + n + " are: " + result);
+//     }
+// }
+
+
+//Q7.Check for Prime Number
+// You are given an integer n. You need to check if the number is prime or not. 
+// Return true if it is a prime number, otherwise return false.
+// A prime number is a number which has no divisors except 1 and itself.
+public class Lec4{
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false; 
         }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; 
+            }
+        }
+        return true; 
+    }
+    public static void main(String[] args) {
+        int n = 7;
+        boolean result = isPrime(n); 
+        System.out.println(n + " is prime: " + result); 
     }
 }
