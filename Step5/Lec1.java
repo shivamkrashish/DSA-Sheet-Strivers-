ab18c8022de4d6870b7542d884ba2aee5056970f
shivamkrashish <shivamkumar120904@gmail.com>
2025-06-06 12:54:07 +0530

    DSA-Sheet-Strivers

--- Step5/Lec1.java
+++ Step5/Lec1.java
@@ -2,18 +2,41 @@
 //You are given a string num, representing a large integer. 
 // Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
 // A substring is a contiguous sequence of characters within a string.
-public class Lec1{
-    public static String largestOddNumber(String num) {
-        for (int i = num.length() - 1; i >= 0; i--) {
-            char ch = num.charAt(i);
-            if ((ch - '0') % 2 == 1) { // Check if the digit is odd
-                return num.substring(0, i + 1);
+// public class Lec1{
+//     public static String largestOddNumber(String num) {
+//         for (int i = num.length() - 1; i >= 0; i--) {
+//             char ch = num.charAt(i);
+//             if ((ch - '0') % 2 == 1) { // Check if the digit is odd
+//                 return num.substring(0, i + 1);
+//             }
+//         }
+//         return "";
+//     }
+//     public static void main(String[] args) {
+//         String num ="547";
+//         System.out.println("The largestOddNum is:"+largestOddNumber(num));
+//     }
+// }
+
+//Q2.Longest Common Prefix
+// Write a function to find the longest common prefix string amongst an array of strings.
+// If there is no common prefix, return an empty string "".
+public class Lec1 {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
             }
         }
-        return "";
+        return prefix;
     }
-    public static void main(String[] args) {
-        String num ="547";
-        System.out.println("The largestOddNum is:"+largestOddNumber(num));
+   public static void main(String[] args) {
+        String[] input = {"flower", "flow", "flight"};
+        String result = longestCommonPrefix(input);
+        System.out.println("Longest Common Prefix: " + result);
     }
 }
+
