8e3a38b3fa7e384e7256696a971b74566962d4b5
shivamkrashish <shivamkumar120904@gmail.com>
2025-06-07 13:59:30 +0530

    DSA-Sheet-Strivers

---
+++ Step5/Lec2.java
@@ -0,0 +1,21 @@
+//Q1.Reverse Words in a String
+// Given an input string s, reverse the order of the words.
+// A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+// Return a string of the words in reverse order concatenated by a single space.
+public class Lec2{
+    public  static String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            result.append(words[i]);
+            if (i != 0) {
+                result.append(" ");
+            }
+        }
+        return result.toString();
+    }
+    public static void main(String[] args) {
+        String str = "shivam is gentlement";
+        System.out.println(reverseWords(str));
+    }
+}
