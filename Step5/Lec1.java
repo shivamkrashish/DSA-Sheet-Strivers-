0f21ab247af64480d094f2995c45dba771a6710a
shivamkrashish <shivamkumar120904@gmail.com>
2025-06-08 18:55:34 +0530

    DSA-Sheet-Strivers

--- Step5/Lec1.java
+++ Step5/Lec1.java
@@ -21,22 +21,51 @@
 //Q2.Longest Common Prefix
 // Write a function to find the longest common prefix string amongst an array of strings.
 // If there is no common prefix, return an empty string "".
-public class Lec1 {
-    public static String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
-        String prefix = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            while (!strs[i].startsWith(prefix)) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) return "";
+// public class Lec1 {
+//     public static String longestCommonPrefix(String[] strs) {
+//         if (strs == null || strs.length == 0) return "";
+//         String prefix = strs[0];
+//         for (int i = 1; i < strs.length; i++) {
+//             while (!strs[i].startsWith(prefix)) {
+//                 prefix = prefix.substring(0, prefix.length() - 1);
+//                 if (prefix.isEmpty()) return "";
+//             }
+//         }
+//         return prefix;
+//     }
+//    public static void main(String[] args) {
+//         String[] input = {"flower", "flow", "flight"};
+//         String result = longestCommonPrefix(input);
+//         System.out.println("Longest Common Prefix: " + result);
+//     }
+// }
+
+//Q3.Remove Outermost Parentheses
+// A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+public class Lec1{
+    public static String removeOuterParentheses(String s) {
+        StringBuilder result = new StringBuilder();
+        int balance = 0;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                if (balance > 0) {
+                    result.append(ch);
+                }
+                balance++;
+            } else {
+                balance--;
+                if (balance > 0) {
+                    result.append(ch);
+                }
             }
         }
-        return prefix;
-    }
-   public static void main(String[] args) {
-        String[] input = {"flower", "flow", "flight"};
-        String result = longestCommonPrefix(input);
-        System.out.println("Longest Common Prefix: " + result);
-    }
-}
 
+        return result.toString();
+    }
+  public static void main(String[]args){
+    String s = "(())";
+    System.out.println(removeOuterParentheses(s));
+
+  }
+}
